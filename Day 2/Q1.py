f72a6bc87f36f4c9a2c03c5bdbe5a4d4ee39667f
Dewyxx <133263985+Dewyxx@users.noreply.github.com>
2023-08-14 19:38:10 +0300

    Delete Day 2 directory

--- Day 2/Q1.py
+++
@@ -1,7 +0,0 @@
-List=[]
-String=input("Enter your sentence:" )
-char=input("Enter your character to locate: ")
-for i, ch in enumerate(String):
-    if ch==char:
-     List.append(i)
-print(List)
--- Day 2/Q3.py
+++
@@ -1,9 +0,0 @@
-names = ["ahmed", "fatma", "Ibrahim"]
-name_dict = {}
-for name in names:
-    first_char = name[0].lower()
-    if first_char in name_dict:
-        name_dict[first_char].append(name)
-    else:
-        name_dict[first_char] = [name]
-print(name_dict)
